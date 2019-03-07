a200e3d9a8ce1a7719c24d422b5e338c4d6b636a
johnshen44 <38093742+johnshen44@users.noreply.github.com>
2019-03-07 15:29:40 +1100

    Update HelloWorldServiceImpl.java

--- helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -32,7 +32,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayHello() {
-        return "Hello World! jontest";
+        return "Hello World! jontestxxx";
     }
 
     @Override
