0ed08d664c8c88b36ad492caa6d33f97442290ad
johnatswin <jon1melb@gmail.com>
2019-04-01 15:44:38 +1100

    test

--- helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -47,7 +47,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayHelloToNames(final List<String> names) {
-        return "jontest2 Hello " + createNameListString(names);
+        return "jontest3 Hello " + createNameListString(names);
     }
 
     /**
